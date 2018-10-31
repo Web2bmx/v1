94a1c789b8e3ffc95674731f346b3fbde452e912
web2bmx <contacto@web2b.mx>
2018-10-30 23:21:04 -0600

    Step order done

--- crea/index.php
+++ crea/index.php
@@ -176,13 +176,13 @@
 				</div>
 				<div class="app-control-step">
 					<div>
-						<h2>Coloca el nombre de tu negocio</h2>
+						<h2>Primero coloca el nombre de tu negocio</h2>
 						<div>	
 							<div>
 								<input type="text" id="inp-content-name" name="inp-content-name" placeholder="Tu primer producto o servicio" /><br />								
 							</div>
 						</div>
-						<h2>ó</h2>
+						<h2 class="middle">ó</h2>
 						<form enctype="multipart/form-data" method="POST" class="file-upload">
 							<p class="one-line-text">
 								<span>
@@ -197,7 +197,7 @@
 				</div>
 				<div class="app-control-step">
 					<div>
-						<h2>¿Tienes un eslogan?</h2>
+						<h2>Ahora coloca tu eslogan</h2>
 						<p class="app-control-step-tooltip-info">Escribe una frase breve que describa tu producto o servicio.</p>		
 						<div>
 						<input type="text" id="inp-content-slogan" name="inp-content-slogan" value="" placeholder="El eslógan de tu negocio" class="optional" />
@@ -206,7 +206,7 @@
 				</div>
 				<div class="app-control-step">
 					<div>
-						<p>Ahora elige la imagen principal para tu página</p>
+						<h2>Elige la imagen de cabecera</h2>
 						<div class="app-control-images">
 							<div id='app-control-images-hero'>
 								<div class="photo-container"></div>
@@ -255,7 +255,7 @@
 								<textarea id="inp-content-item-1" name="inp-content-item-1" placeholder="Tu primer producto o servicio"></textarea>
 							</div>
 						</div>
-						<h2>Elige una imagen para tu principal producto o servicio</h2>
+						<!-- <h2>Elige una imagen para tu principal producto o servicio</h2> -->
 						<div class="app-control-images">
 							<div id='app-control-images-item-1'>
 								<div class="photo-container"></div>
