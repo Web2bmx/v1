6315ea381a44d8dd837a9b54ccbebff4f8cc0658
Antonio Carrillo <antonio.carrillofuentes@gmail.com>
2018-11-12 16:55:08 -0600

    Broke Index into multiple files.

---
+++ crea/include/include_app-control-step_contact-social.php
@@ -0,0 +1,13 @@
+<div>
+						<h2>Escribe las redes sociales de tu Negocio</h2>
+						<div>
+							<div class="inp-cont">
+								<i class="fas fa-pen"></i>
+								<input type="text" id="inp-contact-facebook" name="inp-contact-facebook" value="" placeholder="La página de Facebook de tu Negocio" class="optional" /><br />
+							</div>
+							<div class="inp-cont">
+								<i class="fas fa-pen"></i>
+								<input type="text" id="inp-contact-twitter" name="inp-contact-twitter" value="" placeholder="La página de Twitter de tu Negocio" class="optional" /><br />
+							</div>
+						</div>
+					</div>
