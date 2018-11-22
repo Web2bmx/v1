3a8e04ed9008e90450e0f30fc494e8cd6ef22805
Antonio Carrillo <antonio.carrillofuentes@gmail.com>
2018-11-21 22:29:07 -0600

    Refactoring code.

--- crea/js/creator.js
+++ crea/js/creator.js
@@ -1,51 +1,33 @@
 import validator from "./validator";
 export default function creator () {
     /*GLOBAL VARIABLES*/
-	let windowObjectReference = null,
-		template_id = "",
-		current_step = 0,
-		number_of_items = 1,
-		sample_colors_ready = false,
-		$palettes = null,
-		lastKeyPressed = 0,
-		jd = null,	
-		isNew = false;
-	var new_validator = new validator();
-	var validation = function(){
-		jd = getObjFromLocalStorage("web2b");
-		checkDataValidation();
-    };
-	var init = function() {
-		/*SET UP TEMPLATE*/
-		updateTemplate();
-		/*SET UP APP*/
-		setAppNavigation();
-		setAppSteps();
-		$(window).resize(function(){
-			centerNav();
-			topStepMargin();
-		});
-    };
-	/*APPLICATION FUNCTIONS*/
-	var checkDataValidation = function(){
-		if(Object.keys(jd).length){
-			if(jd.respuestas && Object.keys(jd.respuestas).length < 3){
-				location.href = "/tour";
+	let template_id = "", /* Stores the value of the current template id, needed to compare when it changes, used on updateTemplate*/
+		current_step = 0, /* Stores the value of the current step in the app-control, used on many functions */
+		number_of_items = 1, /*WORK, number of items should come from jd*/
+		$palettes = null, /*Stores the color palettes from XML, used when loading colors and when updating template*/
+		lastKeyPressed = 0, /*Used to compare last pressed key for updating values*/
+		jd = null,	/*Used to store main data object, JasonData*/
+		isNew = false; /*Used to flag whether is a new user */
+	var new_validator = new validator(); /* adds validator helper object, contains string validation capabilities */
+	var validation = function(){/*Public function, invoked before init by scripts*/
+		jd = getObjFromLocalStorage("web2b");/*Retrieves Json Data from local Storage*/
+		if(Object.keys(jd).length){/*If at least one question has been answered*/
+			if(jd.respuestas && Object.keys(jd.respuestas).length < 3){/*Incomplete tour*/
+				location.href = "/tour";/*takes user to the tour*/
 			} else {
-				isNew = true;
+				isNew = true;/*it's a new user (?)*/
 			}
-		} else {
-			let web2bTemplate = getObjFromLocalStorage("web2b_template");
-			if(Object.keys(web2bTemplate).length){
-				jd = web2bTemplate;
+		} else {/*jd has something on it, so it's a returning visitor*/
+			let web2bTemplate = getObjFromLocalStorage("web2b_template");/*get template from local storage*/
+			if(Object.keys(web2bTemplate).length){/*template has any key*/
+				jd = web2bTemplate;/*why does jd gets replaced by webtemplate?*/
 			} else {
-				location.href = "/";
+				location.href = "/";/*takes User to root, why?*/
 			}
 		}
-
-	};
-	var setAppSteps = function () {
-		$(".dialog").dialog({
+    };
+	var init = function() {/*init is public function and is invoked after validate*/
+		$(".dialog").dialog({/*Set .dialog from JQuery UI*/
 			autoOpen: false,
 			modal: true,
 			show: {
@@ -58,14 +40,17 @@ export default function creator () {
 			},
 			closeOnEscape: false
 		  }); 
-		  
-		if(isNew){											
-			$(document).ready(function(){				
-				$(".app-new-start.dialog").dialog( "option", "width", 400 );
-				$(".app-new-start.dialog").dialog( "open" );
-			});
+		if(isNew){/*If is a new User, open Dialog*/											
+			$(".app-new-start.dialog").dialog("option", "width", 400);
+			$(".app-new-start.dialog").dialog("open");
 		}
-
+		updateTemplate();/*Updates template*/
+		setAppNavigation();/*Sets up the navigation for App*/
+		setAppControls();/*Set the controls of the app so they update the template values*/
+		setAppSteps();/*Sets up the steps*/
+	};
+	/*APPLICATION FUNCTIONS*/
+	var setAppControls = function () {
 		$(document.body).on("change", "[name^='inp-'][type!='text']", function() { 
 			updateTemplate();
 		});
@@ -113,7 +98,12 @@ export default function creator () {
 			$this.prop('checked', false);
 			$("#inp-content-item-add-n").trigger("click");
 			centerNav();
+			$(window).resize(function(){
+				centerNav();
+			});
 		});
+	}
+	var setAppSteps = function () {
 		/*TOOLTIPS****/
 		$(".app-control-step-tooltip-info").prev("*").append($(".app-control-step-tooltip.template").clone().removeClass("template"));
 		$(".app-control-step-tooltip").click(function() {
@@ -133,32 +123,10 @@ export default function creator () {
 			url: "xml/sample_colors.xml",
 			dataType: "xml",
 			success: function(data) {
-				let $xml, t; 
+				let $xml; 
 				$xml =$(data);
 				$palettes = $xml.find("palette");
-				t = $palettes.length;
-				for (let i = 0; i < $palettes.length; i++) {
-					var $control_color = $(".template.control-color").clone();
-					var $palette = $palettes.filter(":eq(" + i + ")");
-					$control_color.removeClass("template");
-					$control_color.find("h4").html($palette.find("name").html());
-					$control_color.find("input").attr("id", ("inp-palette-" + i));
-					$control_color.find("input").attr("value", i);
-					$control_color.find(".control-color-thumb").css({ "background-color" : $palette.find("bg>back").html() });
-					var blocks = ["top", "middle", "bottom"];
-					for (let k = 0; k < 3; k++) {
-						$control_color.find(".control-color-thumb-bg:eq(" + k + ")").css({ "background-color" : $palette.find(blocks[k] + ">back").html() });
-						$control_color.find(".control-color-thumb-content:eq(" + k + ")").css({ "background-color" : $palette.find(blocks[k] + ">fore").html() });
-					}
-					$("#app-control-palettes").append($control_color);
-				}
-				$(".control-color-thumb").on('click', function() {
-					var $this = $(this);
-					$this.closest("#app-control-palettes").find(".control-color-thumb").removeClass("thumb-selected");
-					$this.next("input").trigger("click");
-					$this.addClass("thumb-selected");
-				});
-				sample_colors_ready = true;
+				setColors();
 			}
 		});	
 		/* TEMPLATE DESIGN*/
@@ -186,6 +154,30 @@ export default function creator () {
 			$(".alert.dialog").dialog( "close" ); 
 		});		
 	};
+	var setColors = function() {
+		let t = $palettes.length;
+		for (let i = 0; i < $palettes.length; i++) {
+			var $control_color = $(".template.control-color").clone();
+			var $palette = $palettes.filter(":eq(" + i + ")");
+			$control_color.removeClass("template");
+			$control_color.find("h4").html($palette.find("name").html());
+			$control_color.find("input").attr("id", ("inp-palette-" + i));
+			$control_color.find("input").attr("value", i);
+			$control_color.find(".control-color-thumb").css({ "background-color" : $palette.find("bg>back").html() });
+			var blocks = ["top", "middle", "bottom"];
+			for (let k = 0; k < 3; k++) {
+				$control_color.find(".control-color-thumb-bg:eq(" + k + ")").css({ "background-color" : $palette.find(blocks[k] + ">back").html() });
+				$control_color.find(".control-color-thumb-content:eq(" + k + ")").css({ "background-color" : $palette.find(blocks[k] + ">fore").html() });
+			}
+			$("#app-control-palettes").append($control_color);
+		}
+		$(".control-color-thumb").on('click', function() {
+			var $this = $(this);
+			$this.closest("#app-control-palettes").find(".control-color-thumb").removeClass("thumb-selected");
+			$this.next("input").trigger("click");
+			$this.addClass("thumb-selected");
+		});
+	}
 	var setImageSelection = function (ide) {
 		$(".photo-container").html("");
 		//Check if there are already images upload from user
@@ -333,6 +325,9 @@ export default function creator () {
 				$("#app-control").removeClass("view");
 				centerNav();
 				topStepMargin();				
+				$(window).resize(function(){
+					topStepMargin();
+				});
 			}
 			$("body").toggleClass("init");
 		});
@@ -399,55 +394,53 @@ export default function creator () {
 		selections = jd.selections || {};
 		topStepMargin();
 		/*Colores*/
-		if (sample_colors_ready) {
-			let palette_id = 0;
-			if ($("[name='inp-palette']:checked").length == 0) {
-				if (selections["palette"]) {
-					palette_id = selections["palette"].value;
-				} else {
-					selections["palette"] = { "name": "palette", "type": "id", "value":"0" };
-					palette_id = selections["palette"].value;
-				}
-				$(".control-color-thumb:eq(" + palette_id + ")").addClass("thumb-selected");
-				$("[name='inp-palette']:eq(" + palette_id + ")").attr("checked", "checked");
+		let palette_id = 0;
+		if ($("[name='inp-palette']:checked").length == 0) {
+			if (selections["palette"]) {
+				palette_id = selections["palette"].value;
 			} else {
-				palette_id = $("[name='inp-palette']:checked").val();
-				selections["palette"].value = palette_id;
+				selections["palette"] = { "name": "palette", "type": "id", "value":"0" };
+				palette_id = selections["palette"].value;
 			}
-			saveSelected("palette",palette_id,'id');
-			/**/
-			var $palette = $palettes.filter(":eq(" + palette_id + ")");
-			var c1 = $palette.find("top>back").html();
-			var col_hero_content = HexColorToRGBA(c1, 0.6);
-			var c2 = $palette.find("middle>back").html();
-			var col_items = HexColorToRGBA(c2, 0.6);
-			$template.css({
-				"background-color" : $palette.find("bg>back").html()
-			});
-			$template.find("#hero").css({
-				"background-color" : $palette.find("top>back").html(),
-				"color" : $palette.find("top>fore").html()
-			});
-			$template.find("#hero-content").css({
-				"background-color" : $palette.find("top>back").html(),
-				"color" : $palette.find("top>fore").html()
-			});
-			$template.find("#hero-content h1, #hero-content h2").css({
-				"color" : $palette.find("top>fore").html()
-			});
-			$template.find(".items").css({
-				"background-color" : "transparent",
-				"color" : $palette.find("middle>fore").html()
-			});
-			$template.find(".items .item-content").css({
-				"background-color" : $palette.find("middle>back").html(),
-				"color" : $palette.find("middle>fore").html()
-			});
-			$template.find("footer").css({
-				"background-color" : $palette.find("bottom>back").html(),
-				"color" : $palette.find("bottom>fore").html()
-			});
+			$(".control-color-thumb:eq(" + palette_id + ")").addClass("thumb-selected");
+			$("[name='inp-palette']:eq(" + palette_id + ")").attr("checked", "checked");
+		} else {
+			palette_id = $("[name='inp-palette']:checked").val();
+			selections["palette"].value = palette_id;
 		}
+		saveSelected("palette",palette_id,'id');
+		/**/
+		var $palette = $palettes.filter(":eq(" + palette_id + ")");
+		var c1 = $palette.find("top>back").html();
+		var col_hero_content = HexColorToRGBA(c1, 0.6);
+		var c2 = $palette.find("middle>back").html();
+		var col_items = HexColorToRGBA(c2, 0.6);
+		$template.css({
+			"background-color" : $palette.find("bg>back").html()
+		});
+		$template.find("#hero").css({
+			"background-color" : $palette.find("top>back").html(),
+			"color" : $palette.find("top>fore").html()
+		});
+		$template.find("#hero-content").css({
+			"background-color" : $palette.find("top>back").html(),
+			"color" : $palette.find("top>fore").html()
+		});
+		$template.find("#hero-content h1, #hero-content h2").css({
+			"color" : $palette.find("top>fore").html()
+		});
+		$template.find(".items").css({
+			"background-color" : "transparent",
+			"color" : $palette.find("middle>fore").html()
+		});
+		$template.find(".items .item-content").css({
+			"background-color" : $palette.find("middle>back").html(),
+			"color" : $palette.find("middle>fore").html()
+		});
+		$template.find("footer").css({
+			"background-color" : $palette.find("bottom>back").html(),
+			"color" : $palette.find("bottom>fore").html()
+		});
 		/*Content & Contacto*/
 		$("[id^='inp-contact-'], [id^='inp-content-'], #siteName").each(function() {
 			let $this = $(this),
@@ -595,6 +588,7 @@ export default function creator () {
 	/*EO APPLICATION FUNCTIONS*/
 	/*GENERAL FUNCTIONS*/
 	function openRequestedPopup(strUrl, strWindowName) {
+		let windowObjectReference = null;
 		if(windowObjectReference == null || windowObjectReference.closed) {
 		windowObjectReference = window.open(strUrl, strWindowName,
 				"resizable,scrollbars,status");
