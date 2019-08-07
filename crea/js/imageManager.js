adf3628e3054eaa446731f9e4ee06e25bf59c949
Antonio Carrillo <antonio.carrillofuentes@gmail.com>
2019-08-06 19:38:53 -0500

    Gallery updates.

--- crea/js/imageManager.js
+++ crea/js/imageManager.js
@@ -36,113 +36,106 @@ export default function imageManager () {
 			per_page: 20,
 			orientation: 'landscape'
 		}).done(function(data){
-			if (_loaded_images.length == 0) {
-				for(let x=0; x<data.length ; x++){
-					_loaded_images.push(data[x].urls.regular);
-				}
-			}
-			let image_types = ["hero","aboutus","cta","gallery"];
-			let items = $("#template .item").length;
-			for(let i = 1; i <= items; i++ ){ image_types.push('item-' + i); }
-			let current_images = [];
-			let loaded_images = _loaded_images;
-			for (let i = 0; i < image_types.length; i++) {
-				current_images[image_types[i]] = [];
-				if (_ctrl.jd.selections[("#img-" + image_types[i])] && _ctrl.jd.selections[("#img-" + image_types[i])].img) {
-					if (image_types[i] == "gallery") {
-						let jd_images = _ctrl.jd.selections[("#img-" + image_types[i])].img.split(",");
-						for (let j = 0; j < jd_images.length; j++){
-							if (jd_images[j] != "") {
-								current_images[image_types[i]].push([jd_images[j], "selected"]);
-							}
-						}
-					} else {
-						current_images[image_types[i]].push([_ctrl.jd.selections[("#img-" + image_types[i])].img, "selected"]);
-					}
-				}
-			}
-			for (let i = 0; i < loaded_images.length; i++) {
-				for (let j = 0; j < image_types.length; j++) {
-					if(current_images[image_types[j]]) {
-						let add_image = true;
-						for (let k = 0; k < current_images[image_types[j]].length; k++) {
-							if (current_images[image_types[j]][k] == loaded_images[i]) {
-								add_image = false; break;
-							}
-						}
-						if (add_image) { current_images[image_types[j]].push([loaded_images[i], ""]); }
-					}
-				}
-			}
-			for (let i = 0; i < image_types.length; i++) {
-				if(current_images[image_types[i]]) {
-					for(let j=0; j < current_images[image_types[i]].length; j++){
-						let $img_thumb = $(".img-thumb.template").clone();
-						let img = current_images[image_types[i]][j][0];
-						if (img != "") {
-							 
-							$img_thumb.removeClass("template").find(".img-thumb-cont").css({
-								"background-image" : ("url(" + img + ")")
-							}).attr("data-img-url", img);
-							$img_thumb.find("input").attr("value", img);
-							var $this_img_thumb = $img_thumb.clone();
-							$this_img_thumb.find("input").attr("name", ("inp-img-" + image_types[i]));
-							if (current_images[image_types[i]][j][1] == "selected") { $this_img_thumb.addClass("selected"); }
-							$("#app-control-images-" + image_types[i] + " .photo-container").append($this_img_thumb);
-							if (image_types[i] == "gallery") {
-								$this_img_thumb.find(".img-thumb-overlay>span").html(j + 1);
-							}
-						}
-					}
-				}
-			}
-			$(document.body).on('click', '.img-thumb-cont', function() {
-				var $this = $(this);
-				$this.closest("[id^='app-control-images']").find(".img-thumb").removeClass("thumb-selected");
-				$this.next("input").trigger("click");
-				$this.parent().addClass("thumb-selected");
-				/*if($this.closest("#app-control-images-gallery").length > 0) {
-					$this.parent().find("");
-				}*/
-			});	
-			$(document.body).on('click', '.img-thumb-cont-zoom', function() {
-				let img_url = $(this).parent().find(".img-thumb-cont").attr("data-img-url");
-				$("#single-modal>div").html("<img src='" + img_url + "' class='img-thumb-zoomed' />");
-				$("#single-modal").fadeIn();
-			});
-
-			/*
-			let image_types = ["hero","aboutus","cta","gallery"];
-			let items = $("#template .item").length;
-			for(let i = 1; i <= items; i++ ){
-				image_types.push('item-' + i);
-			}
-			for(let x=0; x<data.length ; x++){
-				var $img_thumb = $(".img-thumb.template").clone();
-				$img_thumb.removeClass("template").find(".img-thumb-cont").css({
-					"background-image" : ("url(" + data[x].urls.regular + ")")
-				}).attr("data-img-url", data[x].urls.regular);
-				$img_thumb.find("input").attr("value", data[x].urls.regular);
-				for (let i = 0; i < image_types.length; i++) {
-					var $this_img_thumb = $img_thumb.clone();
-					$this_img_thumb.find("input").attr("name", ("inp-img-" + image_types[i]));
-					$("#app-control-images-" + image_types[i] + " .photo-container").append($this_img_thumb);
-				}
-			}
-			$(document.body).on('click', '.img-thumb-cont', function() {
-				var $this = $(this);
-				$this.closest("[id^='app-control-images']").find(".img-thumb").removeClass("thumb-selected");
-				$this.next("input").trigger("click");
-				$this.parent().addClass("thumb-selected");
-			});	
-			$(document.body).on('click', '.img-thumb-cont-zoom', function() {
-				let img_url = $(this).parent().find(".img-thumb-cont").attr("data-img-url");
-				$("#single-modal>div").html("<img src='" + img_url + "' class='img-thumb-zoomed' />");
-				$("#single-modal").fadeIn();
-			});
-			*/
+			onImagesLoaded(data);
 		}).fail(function(){});
 	};
+	var onImagesLoaded = function (data){
+		if (_loaded_images.length == 0) {
+			for(let x=0; x<data.length ; x++){
+				_loaded_images.push(data[x].urls.regular);
+			}
+		}
+		sortImages();
+	}
+	var sortImages = function (){
+		let image_types = ["hero","aboutus","cta","gallery"];
+		let items = $("#template .item").length;
+		for(let i = 1; i <= items; i++ ){ image_types.push('item-' + i); }
+		for (let i = 0; i < image_types.length; i++) {
+			let current_images = [];
+			current_images[image_types[i]] = [];
+			if(image_types[i] == "gallery") {
+				sortImagesMultiple(image_types[i], current_images);
+			} else {
+				sortImagesSingle(image_types[i], current_images);
+			}
+		}
+		$(document.body).on('click', '.img-thumb-cont', function() {
+			var $this = $(this);
+			$this.closest("[id^='app-control-images']").find(".img-thumb").removeClass("thumb-selected");
+			$this.next("input").trigger("click");
+			$this.parent().addClass("thumb-selected");
+		});	
+		$(document.body).on('click', '.img-thumb-cont-zoom', function() {
+			let img_url = $(this).parent().find(".img-thumb-cont").attr("data-img-url");
+			$("#single-modal>div").html("<img src='" + img_url + "' class='img-thumb-zoomed' />");
+			$("#single-modal").fadeIn();
+		});
+	}
+	var sortImagesSingle = function (t, current_images){
+		if (_ctrl.jd.selections[("#img-" + t)] && _ctrl.jd.selections[("#img-" + t)].img) {
+			current_images[t].push([_ctrl.jd.selections[("#img-" + t)].img, "selected"]);
+		}
+		for (let i = 0; i < _loaded_images.length; i++) {
+			if(current_images[t]) {
+				let add_image = true;
+				for (let k = 0; k < current_images[t].length; k++) {
+					if (current_images[t][k][0] == _loaded_images[i]) {
+						add_image = false; break;
+					}
+				}
+				if (add_image) { current_images[t].push([_loaded_images[i], ""]); }
+			}
+		}
+		if(current_images[t]) {
+			for(let j=0; j < current_images[t].length; j++){
+				setThumb(t, j, current_images);
+			}
+		}
+	}
+	var sortImagesMultiple = function (t, current_images){
+		if (_ctrl.jd.selections[("#img-" + t)] && _ctrl.jd.selections[("#img-" + t)].img) {
+			let jd_images = _ctrl.jd.selections[("#img-" + t)].img.split(",");
+			for (let j = 0; j < jd_images.length; j++){
+				if (jd_images[j] != "") {
+					current_images[t].push([jd_images[j], "selected"]);
+				}
+			}
+		}
+		for (let i = 0; i < _loaded_images.length; i++) {
+			if(current_images[t]) {
+				let add_image = true;
+				for (let k = 0; k < current_images[t].length; k++) {
+					if (current_images[t][k][0] == _loaded_images[i]) {
+						add_image = false; break;
+					}
+				}
+				if (add_image) { current_images[t].push([_loaded_images[i], ""]); }
+			}
+		}
+		if(current_images[t]) {
+			for(let j=0; j < current_images[t].length; j++){
+				setThumb(t, j, current_images);
+			}
+		}
+	}
+	var setThumb = function (t, j, current_images){
+		let $img_thumb = $(".img-thumb.template").clone();
+		let img = current_images[t][j][0];
+		if (img != "") {
+			$img_thumb.removeClass("template").find(".img-thumb-cont").css({
+				"background-image" : ("url(" + img + ")")
+			}).attr("data-img-url", img);
+			$img_thumb.find("input").attr("value", img);
+			var $this_img_thumb = $img_thumb.clone();
+			$this_img_thumb.find("input").attr("name", ("inp-img-" + t));
+			if (current_images[t][j][1] == "selected") { $this_img_thumb.addClass("selected"); }
+			$("#app-control-images-" + t + " .photo-container").append($this_img_thumb);
+			if (t == "gallery") {
+				$this_img_thumb.find(".img-thumb-overlay>span").html(j + 1);
+			}
+		}
+	}
 	var uploadImage = function (e){
 		e.preventDefault();
 		var f = $("input[type=file]",e.currentTarget),
