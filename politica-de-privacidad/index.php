dd407857298188f4978f73087a450a408c473384
Antonio Carrillo <antonio.carrillofuentes@gmail.com>
2020-02-09 23:03:14 -0600

    Privacy policy

---
+++ politica-de-privacidad/index.php
@@ -0,0 +1,129 @@
+<!doctype html>
+	<html lang="en">
+		<head>
+			<!--
+			<script type="text/javascript" id="inspectletjs" src="js/inspectlet.js"></script>
+			<script async src="https://www.googletagmanager.com/gtag/js?id=UA-108388790-1"></script>
+			<script type="text/javascript" id="inspectletjs" src="js/gdatalayer.js"></script>
+			-->
+			<meta name="viewport" content="width=device-width, user-scalable=no">
+			<meta charset="utf-8">
+			<title>Web2b - Impulsa tu estrategia digital</title>
+			<meta name="description" content="Impulsa tu estrategia digital">
+			<meta name="author" content="Web2b">
+			<link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
+			<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+			<link rel="stylesheet" href="../css/jquery-ui.min.css" >
+			<link rel="stylesheet" href="../css/jquery-ui.structure.min.css" >
+			<link rel="stylesheet" href="..//css/jquery-ui.theme.min.css" >
+			<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/solid.css" integrity="sha384-VGP9aw4WtGH/uPAOseYxZ+Vz/vaTb1ehm1bwx92Fm8dTrE+3boLfF1SpAtB1z7HW" crossorigin="anonymous">
+			<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/fontawesome.css" integrity="sha384-1rquJLNOM3ijoueaaeS5m+McXPJCGdr5HcA03/VHXxcp2kX2sUrQDmFc3jR5i/C7" crossorigin="anonymous">
+			<link rel="stylesheet" href="../landing/css/styles.css?v=1.0">
+			<!--[if lt IE 9]>
+				<script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.js"></script>
+			<![endif]-->
+		</head>
+		<body>
+			<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+			<header>
+				<div id="branding"><img id="logo" src="../landing/img/logo_web2b.svg" /></div>
+			</header>
+			<nav role="navigation">
+				<div id="menuToggle">
+					<input type="checkbox" /><span></span><span></span><span></span>
+					<div id="menu">
+						<ul>
+							<li><a href="#hero">Inicio</a></li>
+							<li><a href="/blog">Blog</a></li>
+							<li><a href="#steps">¿Cómo funciona?</a></li>
+							<li><a href="#services">Nuestros servicios</a></li>
+							<li><a href="/tour">Comenzar</a></li>
+							<li><a class="login-btn" href="">Ingreso</a></li>
+						</ul>
+					</div>
+				</div>
+			</nav>
+			<main>
+				<section id="politica">
+				<h1>DECLARACIÓN DE PRIVACIDAD</h1>
+				<p>Fecha de última modificación: 9 de febrero de 2020.</p>
+                <h2>SECCIÓN 1 - ¿QUÉ HACEMOS CON TU INFORMACIÓN?</h2>
+                <p>Cuando compras algo de nuestro sitio, como parte del proceso de compra venta, nosotros recolectamos la información personal que nos das tales como nombre, dirección y correo electrónico.</p>
+                <p>Cuando navegas en nuestro sitio, también recibimos de manera automática la dirección de protocolo de internet de tu computadora (IP) con el fin de proporcionarnos información que nos ayuda a conocer acerca de su navegador y sistema operativo.</p>
+                <p>Email marketing: Con tu permiso, podremos enviarte correos electrónicos acerca de nuestro sitio, nuevos servicios y otras actualizaciones.</p>
+                <h2>SECTION 2 - CONSENTIMIENTO</h2>
+                <h3>¿Cómo obtienen mi consentimiento?</h3>
+                <p>Cuando nos provees tu información personal para completar una transacción, verificar tu tarjeta de crédito, crear una órden,  concertar un envío o hacer una devolución, implicamos que aceptas la recolección y uso por esa razón específica solamente.</p>
+                <p>Si te pedimos tu información personal por una razón secundaria, como marketing, te pediremos directame tu expreso consentimiento, o te daremos la oportunidad de negarte.</p>
+                <h3>¿Cómo puedo anular mi consentimiento?</h3>
+                <p>Si luego de haber aceptado cambias de opinión, puedes anular tu consentimiento para que te contactemos, por la recolección, uso o divulgación de tu información, en cualquier momento, contactándonos a <a href="mailto:privacidad@web2b.mx" target="_blank>">privacidad@web2b.mx</a> o escribiéndonos a: Web2B, C. Canutillo 3010 Fraccionamiento Revolución, Tlaquepaque, Jalisco, CP 45580, Mexico</p>
+                <h2>SECCIÓN 3 - DIVULGACIÓN</h2>
+                <p>Podemos divulgar tu información personal si se nos requiere por ley o si violas nuestros Términos de Servicio.</p>
+                <h2>SECCIÓN 4 - Almacenamiento</h2>
+                <p>Nuestro sitio se encuentra alojado en <a href="https://www.hostmonster.com/" target="_blank">HostMonster.com</a>. Ellos nos proporcionan servicios de dominio y hospedaje.</p>
+                <p>Tus datos se almacenan en un servidor seguro detrás de un firewall. Puedes conocer más al respecto en la <a href="https://www.endurance.com/privacy/privacy" target="_blank">página de Privacidad de HostMonster.com</a></p>
+                <h3>Pagos:</h3>
+                <p>Web2b.mx no almacena ni procesa información sobre tu tarjeta de crédito, si tú decides realizar una compra en web2b.mx tu pago será procesado por <a href="https://www.paypal.com/mx/home" target="_blank">PayPal.com</a></p>
+                <p>Para una visión más clara, es posible que también desees leer las <a href="https://www.paypal.com/mx/webapps/mpp/paypal-buyer-protection" target="_blank">Condiciones de servicio de PayPal aquí</a> y su <a href="https://www.paypal.com/mx/webapps/mpp/ua/privacy-full?locale.x=es_MX" target="_blank">Declaración de privacidad aquí.</a></p>
+                <h2>SECCIÓN 5 - SERVICIOS DE TERCERAS PARTES</h2>
+                <p>En general, los proveedores de terceras partes utilizados por nosotros solo recopilarán, usarán y divulgarán tu información en la medida que sea necesaria para que les permita desempeñar los servicios que nos proveen.</p>
+                <p>Sin embargo, algunos proveedores de servicios de terceros, tales como pasarelas de pago y otros procesadores de transacciones de pago, tienen sus propias poliíticas de privacidad con respecto a la información que estamos obligados a proporcionarles para las transacciones relacionadas con las compras.</p>
+                <p>Para estos proveedores, te recomendamos que leas las poliíticas de privacidad para que puedas entender la manera en que tu información personal será manejada.</p>
+                <p>En particular, recuerda que algunos proveedores pueden estar ubicados o contar con instalaciones que se encuentran en una jurisdicción diferente a ti o nosotros.  Asi que si deseas proceder con una transacción que involucra los servicios de un proveedor a terceros, tu información puede estar sujeta a las leyes de la jurisdicción (jurisdicciones) en que se encuentra el proveedor de servicios o sus instalaciones.</p>
+                <p>A modo de ejemplo, si te encuentras en Canadá y tu transacción es procesada por una pasarela de pago con sede en Estados Unidos, entonces tu información personal utilizada para completar la transacción puede ser sujeto de divulgación en virtud de la legislación de Estados Unidos, incluyendo la Ley Patriota.</p>
+                <p>Una vez que abandonas nuestro sitio web o te rediriges a un sitio o aplicación de terceros, ya no estás siendo regulados por la presente Política de Privacidad o los Términos de Servicio de nuestra página web.</p>
+                <h3>Enlaces</h3>
+                <p>Cuando haces clic en enlaces de nuestro sitio, puede que seas redirigido fuera de nuestro sitio.  No somos reponsables por las prácticas de privacidad de otros sitios y te recomendamos leer sus normas de privacidad.</p>
+                <h2>SECCIÓN 6 - SEGURIDAD</h2>
+                <p>Para proteger tu información personal, tomamos precauciones razonables y seguimos las mejores prácticas de la industria para asegurarnos de que no haya pérdida de manera inapropiada, mal uso, acceso, divulgación, alteración o destrucción de la misma.</p>
+                <p>Si nos proporcionas la información de tu tarjeta de crédito, dicha información es encriptada mediante la tecnología Secure Socket Layer (SSL) y se almacena con un cifrado AES-256.  Aunque ningún método de transmisión a través de Internet o de almacenamiento electrónico es 100% seguro, seguimos todos los requisitos de PCI-DSS e implementamos normas adicionales aceptadas por la industria.</p>
+                <h3>COOKIES</h3>
+                <p>Aquí tienes una lista de cookies que utilizamos. Las enlistamos para que puedas elegir si quieres optar por quitarlas o no.</p>
+                <ul>
+                    <li>__zlcmid: Esta cookie es utilizada por nuestro servicio de chat en línea provisto por Zopim.com para identificarte como un Visitante recurrente.</li>
+                    <li>_ga: Esta cookie es utilizada por nuestro servicio de Analíticas web, Google Analytics. Se usa para distinguir a los usuarios.</li>
+					<li>_gid: Esta cookie es utilizada por nuestro servicio de Analíticas web, Google Analytics. Se usa para distinguir a los usuarios.</li>
+					<li>_gat: Esta cookie es utilizada por nuestro servicio de Analíticas web, Google Analytics. Se usa para limitar el porcentaje de solicitudes. Si se ha implementado Google Analytics mediante Google Tag Manager, esta cookie se llamará _dc_gtm_property-id.</li>
+					<li>AMP_TOKEN: Esta cookie es utilizada por nuestro servicio de Analíticas web, Google Analytics. Incluye un token que se puede utilizar para recuperar un ID de cliente del servicio de IDs de cliente de AMP. Otros posibles valores indican inhabilitaciones, solicitudes en curso o errores obtenidos al recuperar un IDs del servicio de IDs de cliente de AMP.</li>
+					<li>_gac_property-id: Esta cookie es utilizada por nuestro servicio de Analíticas web, Google Analytics. Incluye información de la campaña relativa al usuario. Si has vinculado tus cuentas de Google Analytics y Google Ads, las etiquetas de conversión en sitio web de Google Ads leerán esta cookie, a menos que la inhabilites.</li>
+                </ul>
+                <h2>SECCIÓN 7 - EDAD DE CONSENTIMIENTO</h2>
+                <p>Al utilizar este sitio, declaras que tienes al menos la mayoría de edad en tu estado o provincia de residencia, o que tienes la mayoría de edad en tu estado o provincia de residencia y que nos has dado tu consentimiento para permitir que cualquiera de tus dependientes menores use este sitio.</p>
+                <h2>SECCIÓN 8 - CAMBIOS A ESTA POLÍTICA DE PRIVACIDAD</h2>
+                <p>Nos reservamos el derecho de modificar esta política de privacidad en cualquier momento, asi que por favor revísala frecuentemente.  Cambios y aclaraciones entrarán en vigencia inmediatamente después de su publicación en el sitio web.  Si hacemos cambios materiales a esta política, notificaremos aquí que ha sido actualizada, por lo que cual estás enterado de qué información recopilamos, cómo y bajo qué circunstancias, si las hubiere, la utilizamos y/o divulgamos.</p>
+                <p>Si nuestro sitio es adquirido o fusionado con otra empresa, tu información puede ser transferida a los nuevos propietarios, para que podamos seguir proveyendote de servicios.</p>
+                <h2>PREGUNTAS E INFORMACIÓN DE CONTACTO</h2>
+				<p>Si quieres: acceder, corregir, enmendar o borrar cualquier información personal que poseamos sobre ti, registrar una queja, o simplemente quieres más información contacta a nuestro Oficial de Cumplimiento de Privacidad <a href="mailto:privacidad@web2b.mx" target="_blank">privacidad@web2b.mx</a> o por correo postal a Web2B, C. Canutillo 3010 Fraccionamiento Revolución, Tlaquepaque, Jalisco, 45580, Mexico.</p>
+				</section>
+			</main>	
+			<footer>
+				<div class="container">
+					<nav>
+						<h6>Menú</h6>
+						<ul>
+							<li><a href="#hero">Inicio</a></li>
+							<li><a href="/blog">Blog</a></li>
+							<li><a href="#steps">¿Cómo funciona?</a></li>
+							<li><a href="#services">Nuestros servicios</a></li>
+							<li><a href="/tour">Comenzar</a></li>
+							<li><a class="login-btn">Ingreso</a></li>
+						</ul>
+					</nav>	
+					<nav>
+						<h6>Blog</h6>
+						<ul>
+							<li><a href="/blog/como-puedo-hacer-yo-mismo-mi-pagina-web/">¿Cómo puedo hacer yo mismo mi página web?</a></li>
+							<li><a href="/blog/como-puedo-evaluar-cual-es-el-mejor-servicio-para-crear-mi-propia-pagina-web/">¿Cómo puedo evaluar cuál es el mejor servicio para crear mi propia página web?</a></li>
+							<li><a href="/blog/quien-es-la-persona-indicada-para-ayudarme-a-crear-mi-pagina-web/">¿Quién es la persona indicada para ayudarme a crear mi página web?</a></li>
+						</ul>
+					</nav>	
+					<div id="social">
+						<ul>
+							<li><a href="https://www.facebook.com/web2bmx/" target="_blank"><img src="../landing/img/facebook.png" /> <span>Síguenos en Facebook</span></a></li>
+						</ul>
+					</div>
+				</div>
+				<div id="copyright"><p>&copy;2019 Web2b Todos los derechos reservados</p></div>
+			</footer>
+		</body>
+	</html>
