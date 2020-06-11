1fc7f01858033803c2acebe2079a9c721ce24a0f
Antonio Carrillo <antonio.carrillofuentes@gmail.com>
2020-06-10 22:29:35 -0500

    Title changes

--- crea/include/include_app-control-step_title.php
+++ crea/include/include_app-control-step_title.php
@@ -1,16 +1,16 @@
 <div class="brand-name">
-	<h2>¿Cómo quieres presentar tu marca?</h2>
-	<p>Puedes activar cualquiera de las 2 opciones, o ambas.</p>
+	<h2>Elige mostrar el nombre de tu Negocio o tu logotipo</h2>
 	<div>
 		<p class="form-error">No es posible realizar esta operacion, al menos una de las 2 opciones debe estar seleccionada.</p>
 		<aside>
-			<h4>Escribe el nombre de tu negocio<a class="toogle"><i class="fas fa-toggle-on"></i></a></h4>
+			<p>Escribe el nombre de tu negocio:</p>
 			<div class="inputWihIcons">
 				<i class="fas fa-font"></i><input type="text" id="inp-content-name" name="inp-content-name" placeholder="Nombre de tu negocio" />
 			</div>
+			<h4><a class="toogle"><i class="fas fa-toggle-on"></i></a>&nbsp;Si lo prefieres puedes no mostrarlo.</h4>
 		</aside>
 		<aside>
-			<h4>Selecciona tu logo<a class="toogle"><i class="fas fa-toggle-on"></i></a></h4>
+			<h4><a class="toogle"><i class="fas fa-toggle-on"></i></a>&nbsp;¿Prefieres usar un logotipo?</h4>
 			<form enctype="multipart/form-data" method="POST" class="file-upload">
 				<p class="one-line-text">
 					<span>Sube tu logotipo en formato png o jpg con un mínimo de 400px</span>
