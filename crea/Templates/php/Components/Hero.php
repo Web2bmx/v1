fcb10cc605dfa6090a5d2d6306c9af072fbc237d
cifrandoideas <carlos@cifrandoideas.com>
2020-09-01 17:53:40 -0500

    showing images and logo

--- crea/Templates/php/Components/Hero.php
+++ crea/Templates/php/Components/Hero.php
@@ -2,11 +2,11 @@
 	<div class="wrapper">
 		<div id='img-hero' class="img <?php echo(getImgClasses($content->hero[0]->img[0])); ?>" style="background-image: url(<?php echo($content->hero[0]->img[0]); ?>);"></div>
 		<div class="wrapper-content">
-			<?php if ($content->hero[0]->logotipo != "") : ?>
+			
 				<div id="branding">
 					<img id="img-logo" src="<?php echo($content->hero[0]->logotipo); ?>" alt="Logotipo" />
 				</div>	
-			<?php endif; ?>
+			
 			<h1 id="val-content-name"><?php echo($content->hero[0]->title); ?></h1>
 			<h3 id="val-content-slogan"><?php echo($content->hero[0]->slogan); ?></h3>
 			<?php if ($content->hero[0]->button != "") : ?>
